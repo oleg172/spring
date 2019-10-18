1d140981093f0cc0958f12921f9609f49fbd6706
olmi0615 <oleg.miliaev@netcracker.com>
2019-10-18 14:00:32 +0300

    Initial commit

---
+++ spring-mvc/spring-mvc-xml/src/main/java/com/springmvc/xml/HelloController.java
@@ -0,0 +1,19 @@
+package com.springmvc.xml;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) {
+
+        ModelAndView model = new ModelAndView("hello");
+        model.addObject("msg", "hello world");
+
+        return model;
+    }
+}
