986a8808d0a5ef4205ac89162aa225c1b29d7733
olmi0615 <oleg.miliaev@netcracker.com>
2019-11-19 12:16:31 +0300

    [spring-blog] fix work created date and updated date - add auditing

--- spring-boot-blog/src/main/java/com/spring/blog/Application.java
+++ spring-boot-blog/src/main/java/com/spring/blog/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableJpaAuditing
 @EnableConfigurationProperties(AppProperties.class)
 public class Application {
 
--- spring-boot-blog/src/main/java/com/spring/blog/model/BaseModel.java
+++ spring-boot-blog/src/main/java/com/spring/blog/model/BaseModel.java
@@ -1,17 +1,20 @@
 package com.spring.blog.model;
 
-import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.util.Date;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) //need to work @CreatedDate and @LastModifiedDate
 @Data
 public class BaseModel {
 
